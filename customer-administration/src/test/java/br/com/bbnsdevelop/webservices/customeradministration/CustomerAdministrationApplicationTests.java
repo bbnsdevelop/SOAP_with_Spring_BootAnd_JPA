c4829e63b68504010967a61d0c67f9dbe88d99c5
bbnsdevelop <bbnsdevelop@gmail.com>
2020-08-04 17:40:28 -0300

    initialized project

---
+++ customer-administration/src/test/java/br/com/bbnsdevelop/webservices/customeradministration/CustomerAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bbnsdevelop.webservices.customeradministration;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
