7c190fbd8c43ac236101a66088ac73284a9ef15a
bbnsdevelop <bbnsdevelop@gmail.com>
2020-08-05 16:55:53 -0300

    class generated by jaxb2

--- customer-administration/src/test/java/br/com/bbnsdevelop/webservices/customeradministration/CustomerAdministrationApplicationTests.java
+++ customer-administration/src/test/java/br/com/bbnsdevelop/webservices/customeradministration/CustomerAdministrationApplicationTests.java
@@ -1,13 +1,10 @@
 package br.com.bbnsdevelop.webservices.customeradministration;
 
-import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CustomerAdministrationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
