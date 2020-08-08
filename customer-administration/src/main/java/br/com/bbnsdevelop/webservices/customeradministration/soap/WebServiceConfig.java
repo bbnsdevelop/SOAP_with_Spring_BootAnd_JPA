2d59912d87a988d467e3d0693527395f438cd8b0
bbnsdevelop <bbnsdevelop@gmail.com>
2020-08-08 19:51:18 -0300

    configuration XsdSchema WSDL

--- customer-administration/src/main/java/br/com/bbnsdevelop/webservices/customeradministration/soap/WebServiceConfig.java
+++ customer-administration/src/main/java/br/com/bbnsdevelop/webservices/customeradministration/soap/WebServiceConfig.java
@@ -4,9 +4,13 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
 @Configuration
 @EnableWs
@@ -20,5 +24,21 @@ public class WebServiceConfig extends WsConfigurerAdapter{
 		
 		messageDispatcherServlet.setTransformWsdlLocations(true);
 		return new ServletRegistrationBean<MessageDispatcherServlet>(messageDispatcherServlet, "/ws/*");
+	}
+	
+	@Bean
+	public XsdSchema customerSchema() {		
+		return new SimpleXsdSchema(new ClassPathResource("customer-information.xsd"));
+	}
+	
+	@Bean(name = "customers")
+	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema customerSchema) {
+		DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+		definition.setPortTypeName("CustomerPort");
+		definition.setTargetNamespace("http://customer.customeradministration.webservices.bbnsdevelop.com.br");
+		definition.setLocationUri("/ws");
+		definition.setSchema(customerSchema);
+		
+		return definition;
 	}
 }
