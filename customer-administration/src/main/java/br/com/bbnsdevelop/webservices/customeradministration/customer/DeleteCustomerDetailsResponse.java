3a3988a724484766888b0876356775d29af31565
bbnsdevelop <bbnsdevelop@gmail.com>
2020-08-12 18:08:16 -0300

    add security

--- customer-administration/src/main/java/br/com/bbnsdevelop/webservices/customeradministration/customer/DeleteCustomerDetailsResponse.java
+++ customer-administration/src/main/java/br/com/bbnsdevelop/webservices/customeradministration/customer/DeleteCustomerDetailsResponse.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2020.08.11 às 09:58:56 PM BRT 
+// Gerado em: 2020.08.12 às 05:51:15 PM BRT 
 //
 
 
--- customer-administration/src/main/java/br/com/bbnsdevelop/webservices/customeradministration/soap/WebServiceConfig.java
+++ customer-administration/src/main/java/br/com/bbnsdevelop/webservices/customeradministration/soap/WebServiceConfig.java
@@ -1,5 +1,9 @@
 package br.com.bbnsdevelop.webservices.customeradministration.soap;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +11,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.wss4j2.callback.SimplePasswordValidationCallbackHandler;
+import org.springframework.ws.soap.security.xwss.XwsSecurityInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
@@ -41,4 +48,27 @@ public class WebServiceConfig extends WsConfigurerAdapter{
 		
 		return definition;
 	}
+	
+	@Bean
+	public XwsSecurityInterceptor securityInterceptor(){
+		XwsSecurityInterceptor  securityInterceptor = new XwsSecurityInterceptor();
+		securityInterceptor.setCallbackHandler(callbackHandler());
+		securityInterceptor.setPolicyConfiguration(new ClassPathResource("securityPolicy.xml"));
+		return securityInterceptor;
+		
+	}
+	
+	@Bean
+	public SimplePasswordValidationCallbackHandler callbackHandler() {
+		SimplePasswordValidationCallbackHandler callbackHandler = new SimplePasswordValidationCallbackHandler();
+		callbackHandler.setUsersMap(Collections.singletonMap("teste", "1234"));
+		
+		return callbackHandler;
+	}
+	
+	@Override
+	public void addInterceptors(List<EndpointInterceptor> interceptors) {		
+		interceptors.add(securityInterceptor());
+	}
+	
 }
